17722d9a743c681b55f33062b01bb401718a3ea8
micpiwo <micpiwo@hotmail.fr>
2019-07-11 11:41:03 +0200

    Signed-off-by: micpiwo <micpiwo@hotmail.fr>Ajout de angular + material Signed-off-by: micpiwo <micpiwo@hotmail.fr>Ajout du service font pour communiquer avec le back

--- smoothie-fe/src/app/smoothie.service.ts
+++ smoothie-fe/src/app/smoothie.service.ts
@@ -1,12 +1,36 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../environments/environment';
-import { HttpClient } from '@angular/common/http';
+
 import { Router } from '@angular/router';
+/*Pour utilise le services doit etre subscibe*/
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
 
 /*INTERFACE en dehors de la classe*/
 /*Appel d'un smoothie*/
 export interface Smoothie{
-
+  _id?:String;
+  title?:String;
+ingredients?: [
+    {
+    nom?:String;
+    quantite?:String;   
+    }
+];
+features?: [
+      {
+       cost?:String;
+       prepareTime?:String;
+       }
+];
+advice?:String;
+description?:String;
+steps?:[
+    {
+        stepText?:String;
+    }    
+]; 
 }
 
 @Injectable({
@@ -14,14 +38,21 @@ export interface Smoothie{
 })
 export class SmoothieService {
   /*Appel du fichier environment.ts et de la fonction apiURL => localhost 3000 connexion au backend*/
-  private environment = environment.apiUrl;
+  private apiUrl = environment.apiUrl;
 
+  
   constructor( 
     /*Appel de donnée angular des imports*/
-    private http = HttpClient,
-    private router = Router) 
-    { 
+    private http: HttpClient,
+    private router: Router)
+    { }
+    // Get all Smoothie
+  getSmoothies(): Observable<Smoothie[]> {
+    const smoothieListUrl = '${this.apiUrl}/listes';
+    return this.http.get<Smoothie[]>(smoothieListUrl);
+  }
+
+    
+  }
 
 
-    }
-}
